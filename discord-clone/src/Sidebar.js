b9ffc9af61d641754c272649a0315d099074390a
Kehan <kran0017@student.monash.edu>
2023-07-10 13:21:55 +1000

    add dropdown menu for text channels

--- discord-clone/src/Sidebar.js
+++ discord-clone/src/Sidebar.js
@@ -1,92 +1,129 @@
-import React, {useEffect, useState} from 'react'
-import './Sidebar.css';
+import React, { useEffect, useState } from "react";
+import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarChannel from "./SidebarChannel";
 import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import CallIcon from "@material-ui/icons/Call";
-import { Avatar } from "@material-ui/core";
+import {
+  Avatar,
+  Collapse,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
 import MicIcon from "@material-ui/icons/Mic";
 import HeadsetIcon from "@material-ui/icons/Headset";
 import SettingsIcon from "@material-ui/icons/Settings";
 import { useSelector } from "react-redux";
-import { selectUser } from './features/userSlice';
-import db, { auth } from "./firebase"
-
-
+import { selectUser } from "./features/userSlice";
+import db, { auth } from "./firebase";
 
 function Sidebar() {
-    const user = useSelector(selectUser);
-    const [channels, setChannels] = useState([]);
+  const user = useSelector(selectUser);
+  const [channels, setChannels] = useState([]);
 
-    useEffect(() => {
-        db.collection('channels').onSnapshot(snapshot => (
-            setChannels(snapshot.docs.map(doc => ({
-                id: doc.id,
-                channel: doc.data(),
-            })))
-        ));
-    }, []);
+  const [isChannelsOpen, setIsChannelsOpen] = useState(true);
 
-    const handleAddChannel = () => {
-        const channelName = prompt("Enter a new channel name");
+  useEffect(() => {
+    db.collection("channels").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          channel: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
-        if (channelName) {
-            db.collection('channels').add({
-                channelName: channelName,
-            });
-        }
-    };
+  const handleAddChannel = () => {
+    const channelName = prompt("Enter a new channel name");
+
+    if (channelName) {
+      db.collection("channels").add({
+        channelName: channelName,
+      });
+    }
+  };
+
+  const handleChannelsToggle = () => {
+    setIsChannelsOpen(!isChannelsOpen);
+  };
 
   return (
-    <div className='sidebar'>
-        <div className='sidebar__top'>
-            <h3>My Server</h3>
-            <ExpandMoreIcon />
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>My Server</h3>
+        <ExpandMoreIcon />
+      </div>
+
+
+        <div className="sidebar__channels">
+          <div className="sidebar__channelsHeader">
+            <div className="sidebar__header">
+              <ExpandMoreIcon
+                onClick={handleChannelsToggle}
+                className={`sidebar__expandIcon ${
+                  isChannelsOpen ? "open" : ""
+                }`}
+              />
+              <h4 onClick={handleChannelsToggle}
+                className={`sidebar__expandIcon ${
+                  isChannelsOpen ? "open" : ""
+                }`}>Text Channels</h4>
+            </div>
+
+            <AddIcon
+              onClick={handleAddChannel}
+              className="sidebar__addChannel"
+            />
+          </div>
+
+          <Collapse in={isChannelsOpen}>
+                <List component='div' className="sidebar_channelsList">
+                    {channels.map(({ id, channel }) => (
+                    <SidebarChannel
+                        key={id}
+                        id={id}
+                        channelName={channel.channelName}
+                    />
+                    ))}
+                </List>
+            </Collapse>
         </div>
 
-        <div className='sidebar__channels'>
-            <div className='sidebar__channelsHeader'>
-                <div className='sidebar__header'>
-                    <ExpandMoreIcon />
-                    <h4>Text Channels</h4>
-                </div>
-
-                <AddIcon onClick={handleAddChannel} className='sidebar__addChannel' />
-
-            </div>
-            <div className='sidebar_channelsList'>
-                {channels.map(({ id, channel }) => (
-                    <SidebarChannel key={id} id={id} channelName={channel.channelName} />
-                ))}
-            </div>
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
         </div>
-
-        <div className='sidebar__voice'>
-            <SignalCellularAltIcon className='sidebar__voiceIcon' fontSize='large' />
-            <div className='sidebar__voiceInfo'>
-                <h3>Voice Connected</h3>
-                <p>Stream</p>
-            </div>
-            <div sidebar__voiceIcons>
-                <InfoOutlinedIcon />
-                <CallIcon />
-            </div>
+        <div sidebar__voiceIcons>
+          <InfoOutlinedIcon />
+          <CallIcon />
         </div>
+      </div>
 
-        <div className='sidebar__profile'>
-            <Avatar className='sidebar__avatarPhoto' onClick={() => auth.signOut()} src={user.photo} />
-            <div className='sidebar__profileInfo'>
-                <h3>{user.displayName}</h3>
-                <p>#{user.uid.substring(0,5)}</p>
-            </div>
-            <div className='sidebar__profileIcons'>
-                <MicIcon />
-                <HeadsetIcon />
-                <SettingsIcon />
-            </div>
+      <div className="sidebar__profile">
+        <Avatar
+          className="sidebar__avatarPhoto"
+          onClick={() => auth.signOut()}
+          src={user.photo}
+        />
+        <div className="sidebar__profileInfo">
+          <h3>{user.displayName}</h3>
+          <p>#{user.uid.substring(0, 5)}</p>
         </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
     </div>
   );
 }
