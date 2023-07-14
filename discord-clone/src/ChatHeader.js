8a0b517e9b2c87f2e9f2eeb975efe08bda33d88a
Kehan <kran0017@student.monash.edu>
2023-07-14 22:51:23 +1000

    when people icon is clicked, toggle list of users

--- discord-clone/src/ChatHeader.js
+++ discord-clone/src/ChatHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ChatHeader.css';
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import EditLocationRoundedIcon from "@material-ui/icons/EditLocationRounded";
@@ -6,9 +6,29 @@ import PeopleAltRoundedIcon from "@material-ui/icons/PeopleAltRounded";
 import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 import SendRoundedIcon from "@material-ui/icons/SendRounded";
 import HelpRoundedIcon from "@material-ui/icons/HelpRounded";
-
+import UserList from './UserList';
+import { useSelector } from "react-redux";
+import {
+    Collapse,
+    List,
+  } from "@material-ui/core";
+import { selectUser } from "./features/userSlice";
+import Login from './Login';
 
 function ChatHeader({ channelName }) {
+    const user = useSelector(selectUser);
+
+    const [isUserListOpen, setIsUserListOpen] = useState(true);
+    const [loggedInUser, setLoggedInUser] = useState(user);
+
+    const handleUsersToggle = () => {
+        setIsUserListOpen(!isUserListOpen);
+      };
+
+    const handleLogin = (user) => {
+        setLoggedInUser(user);
+    }
+
   return (
     <div className='chatHeader'>
         <div className='chatHeader__left'>
@@ -21,7 +41,10 @@ function ChatHeader({ channelName }) {
         <div className='chatHeader__right'>
             <NotificationsIcon />
             <EditLocationRoundedIcon />
-            <PeopleAltRoundedIcon />
+            <PeopleAltRoundedIcon onClick={handleUsersToggle}
+                className={`chatHeader__peopleIcon ${
+                  isUserListOpen ? "open" : ""
+                }`}/>
 
             <div className='chatHeader__search'>
                 <input placeholder='Search'></input>
@@ -31,6 +54,16 @@ function ChatHeader({ channelName }) {
             <SendRoundedIcon />
             <HelpRoundedIcon />
         </div>
+        <Collapse in={isUserListOpen}>
+                <List component='div' className="chatHeader_usersList">
+                    {user ? (
+                    <UserList users={[loggedInUser]}
+                    />
+                    ): (
+                        <Login onLogin={handleLogin} />
+                    )}
+                </List>
+            </Collapse>
     </div>
   )
 }
---
+++ discord-clone/src/UserList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function UserList({ users }) {
+  return (
+    <div className='userList'>
+        <h2>
+            UserList
+        </h2>
+        <ul>
+            {users && users.map((user) => (
+                <li key={user.id}>{user.displayName}</li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default UserList
